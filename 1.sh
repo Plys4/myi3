266d8a9d7fa07bb0aee3228e603def1dca131d9f
Plys4 <57731017+Plys4@users.noreply.github.com>
2020-06-29 01:14:58 -0300

    Update 1.sh

--- 1.sh
+++ 1.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 cd scripts/
-touch exec_rofi.sh
-chmod +x exec_rofi.sh
-echo "#!/bin/bash" exec_rofi.sh
-echo "rofi -show drun -show-icons -drun-icon-theme Papirus-Dark" exec_rofi.sh
-sudo cp exec_rofi.sh /usr/bin/
-sudo chmod +x /usr/bin/exec_rofi.sh
+touch exec_rofi
+chmod +x exec_rofi
+echo "#!/bin/bash" >> exec_rofi
+echo "rofi -show drun -show-icons -drun-icon-theme Papirus-Dark" >> exec_rofi
+sudo cp exec_rofi /bin/
+sudo chmod +x /bin/exec_rofi
 exec_rofi
