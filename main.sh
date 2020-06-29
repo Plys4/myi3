fe16fa1c01546666150cefce5b0914e81cb6cab0
Plys4 <57731017+Plys4@users.noreply.github.com>
2020-06-29 01:16:49 -0300

    Update main.sh

--- main.sh
+++ main.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 sudo apt install i3wm -y
 sudo apt install i3blocks -y
-sudo apt install build-essencial -y
+sudo apt install build-essential -y
 sudo apt install cmake -y
-sudo apt install autotools -y
+sudo apt install autotools-dev -y
 sudo apt install xorg -y
 sudo apt install xinit -y
 sudo apt install flatpak -y
@@ -12,4 +12,4 @@ sudo apt install xfce4-terminal compton vim rofi -y
 sudo apt install gnome-backgrounds -y
 sudo apt install papirus-icon-theme -y
 mkdir scripts/
-mkdir bin/
+
