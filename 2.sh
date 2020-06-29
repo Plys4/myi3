504af0f79dcdaad79bf129993125d998191c139b
Plys4 <57731017+Plys4@users.noreply.github.com>
2020-06-29 01:17:33 -0300

    Update 2.sh

--- 2.sh
+++ 2.sh
@@ -2,8 +2,8 @@
 
 cd scripts/
 touch i3-exec
-echo "#!/bin/bash" i3-exec
-echo "startx /usr/bin/i3" i3-exec
+echo "#!/bin/bash" >> i3-exec
+echo "startx /usr/bin/i3" >> i3-exec
 sudo cp i3-exec /bin/
 sudo chmod +x /bin/i3-exec
 i3-exec
