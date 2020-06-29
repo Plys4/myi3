227c92fc408714e1a671d3ad04ad6d2442a79f8b
Plys4 <57731017+Plys4@users.noreply.github.com>
2020-06-29 00:38:57 -0300

    Create main.sh

---
+++ main.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+sudo apt install i3wm -y
+sudo apt install i3blocks -y
+sudo apt install build-essencial -y
+sudo apt install cmake -y
+sudo apt install autotools -y
+sudo apt install xorg -y
+sudo apt install xinit -y
+sudo apt install flatpak -y
+sudo apt install feh -y
+sudo apt install xfce4-terminal compton vim rofi -y
+sudo apt install gnome-backgrounds -y
+mkdir scripts/
+mkdir bin/
